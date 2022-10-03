8bb2bfe797f2e81a0499f64f954b2eb9072335ba
Tom Sonvico <tom.sonvico.pro@gmail.com>
2022-10-03 09:25:42 +0200

    finalisation of the app without redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import "./styles/index.scss"
+import { configureStore } from "@reduxjs/toolkit";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./styles/index.scss";
+import rootReducer from "./reducers"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// REDUX
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
-
